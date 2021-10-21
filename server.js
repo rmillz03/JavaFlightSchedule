57da3fea701d9ec42a8e805598fc9e80cb94f7c5
Rick Miller <rmillz03@gmail.com>
2021-10-21 13:01:19 -0400

    update2

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/mainLayout')
 app.use(expressLayouts)
 app.use(express.static('public')) 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => { 
+  res.sendFile(path.join(__dirname + '/client/build/index.html')) 
+});
 app.use(express.urlencoded({ extended: false })) //need to allow pages to access form data
 
 app.use(session({
